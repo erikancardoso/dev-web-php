cc55851a5daa32e630531f14dca8b7b85f5ae67b
erikancardoso <erikancardoso@outlook.com>
2023-11-08 21:21:53 -0300

    function

--- public/teste.php
+++ public/teste.php
@@ -1,11 +1,43 @@
-<?php
-    // usano endif
-    $a = 22;
-    if ($a <= 50):
-        echo "menor que 50";
-    elseif ($a> 50):  
-        echo "maior que 50";
-    else:
-        echo "não é maior nem igual";
-    endif;
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="shortcut icon" href="images/favicon.ico" />
+    <title> Page </title>
+</head>
+
+<body>
+    <?php
+        echo "<h3> endif </h3>";
+        $a = 50;
+        echo "the value entered was $a, so he is ";
+
+        if ($a < 50):
+            echo "less then 50";
+        elseif ($a > 50):  
+            echo "bigger the 50";
+        else:
+            echo "equal to";
+        endif;
+
+    echo "<hr>";
+    echo "<h3> foreach </h3>";
+
+        $cars=["toyota", "gol","wolkswagen","tesla","mercedes","bmw","fiat"];
+        foreach($cars as $car){
+            echo "brand: $car <br>";
+        }
+    echo "<hr>";
+    echo "<h3> function without parameters and without return</h3>";
+        //create to function
+        function message_hello()
+        {
+            echo "Hello,\n";
+            echo "Welcome!";
+        }
+        // use function
+        message_hello();
     ?>
+</body>
+</html>
